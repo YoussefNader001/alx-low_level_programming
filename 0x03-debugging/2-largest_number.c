c874296184c7f693cfc681c20881403c24d01954
root <youssefnader2020@gmail.com>
2023-10-13 14:28:12 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest = a; // Assume 'a' is the largest initially
+	int largest;
 
-    if (b > largest)
-    {
-        largest = b;
-    }
-
-    if (c > largest)
-    {
-        largest = c;
-    }
-
-    return largest;
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+	return (largest);
 }
