3e27e432a4fb562d393bc3b3fc93f43bfd1534f5
YoussefNader001 <youssefnader2020@gmail.com>
2023-11-16 14:58:40 -0800

    Updates

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -15,16 +14,18 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-
-/* Structure definition for a singly linked list node */
-typedef struct list_s {
-    char *str;           /* String data in the node */
-    size_t len;          /* Length of the string */
-    struct list_s *next; /* Pointer to the next node in the list */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
-/* Function prototype for print_list */
-size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void print_first(void);
 
-#endif /* LISTS_H */
+#endif
