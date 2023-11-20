4f444c43aab5911f0058f6c9772a58b257c962ec
YoussefNader001 <youssefnader2020@gmail.com>
2023-11-20 14:42:08 -0800

    .

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@
  * Return: The number of nodes.                                                       
  */
 
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
   size_t count = 0;
 
