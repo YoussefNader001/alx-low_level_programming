040aa2247168c6774ec0e202bc130a217681c5b3
YoussefNader001 <youssefnader2020@gmail.com>
2023-11-21 12:18:36 -0800

    .

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,8 +11,6 @@
  */
 
 listint_t *add_nodeint(listint_t **head, const int n){
-    if(head == NULL)
-        return (NULL);
     listint_t *newNode = malloc(sizeof(listint_t));
     if (newNode == NULL) 
         return (NULL);
