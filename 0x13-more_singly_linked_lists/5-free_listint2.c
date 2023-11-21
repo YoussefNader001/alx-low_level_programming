30af1267591110d2ba68cec5c22da3eff7e2a281
YoussefNader001 <youssefnader2020@gmail.com>
2023-11-21 13:06:28 -0800

    .

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets head to NULL
+ * @head: pointer to the listint_t list
+ * Return: nothing
+ */
+
+
+void free_listint2(listint_t **head){
+    
+    listint_t *temp;
+    temp = *head;
+    free(temp);
+    
+    
+}
